d1bf1168cd29718016c8c3ede3c770032af7606f
Wes Richardet <wes@tetriscodes.com>
2017-11-17 09:37:25 -0600

    Removing postgis from script

--- db.sh
+++ db.sh
@@ -1,5 +1,4 @@
 createuser signal --createdb -h localhost -U postgres
 createdb signal -O signal -h localhost -U postgres
 psql -U postgres -d signal -c "CREATE EXTENSION IF NOT EXISTS pgcrypto" -h localhost
-psql -U postgres -d signal -c "CREATE EXTENSION IF NOT EXISTS postgis" -h localhost
 psql -U signal   -d signal -c "CREATE SCHEMA IF NOT EXISTS signal" -h localhost
